1495451ea2905fac4c78700d56a3a2bcfa9b5130
David González Vila <davidvila99@users.noreply.github.com>
2019-05-23 10:17:03 +0200

    projecte penjat

---
+++ Pedra, paper, tisora/Pedra, paper, tisora/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pedra__paper__tisora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program jugar = new Program();
+            jugar.jugar();
+        }
+        private void jugar() {
+            
+        }
+    }
+}
